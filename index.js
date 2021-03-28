3b325c0532adc2269880c8c9974975768397abda
Aquasin <73415747+Aquasin@users.noreply.github.com>
2021-03-28 13:12:45 +0530

    Changes to index.js

--- index.js
+++ index.js
@@ -15,15 +15,13 @@ var testMin;
 // Functions
 function startTime() {
     testMilli = setInterval(timerMilli, milliSecInterval);
-    // testSec = setInterval(timerSec, secInterval);
-    // testMin = setInterval(timerMin, minInterval);
 }
 
 function timerMilli() {
     if (milliTime < 10) milliTime = "0" + milliTime;
     milliI[0].innerHTML = milliTime;
-    // console.log(milliTime);
     milliTime++;
+
     if (milliTime > 99) {
         milliTime = 0;
         milliI[0].innerHTML = "00";
@@ -35,35 +33,19 @@ function timerMilli() {
         if (secTime > 9) {
             secTime = 0;
             secI[0].innerHTML = "00";
+
             minTime++;
+            if (minTime < 10) minTime = "0" + minTime;
             minI[0].innerHTML = minTime;
-            console.log(minTime);
+
+            // console.log(minTime);
         }
     }
-}
-
-// function timerSec() {
-//     if (secTime < 10) secTime = "0" + secTime;
-//     secI[0].innerHTML = secTime;
-//     console.log(secTime);
-//     // secTime++;
-//     if (secTime > 59) {
-//         secTime = 0;
-//         ++minTime;
-//     }
-// }
-
-function timerMin() {
-    minI[0].innerHTML = minTime;
-    console.log(minTime);
-    // minTime++;
 }
 
 //
 function stopTime() {
     clearInterval(testMilli);
-    // clearInterval(testSec);
-    // clearInterval(testMin);
 }
 
 function resumeTime() {
